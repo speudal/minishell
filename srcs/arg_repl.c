736d6551860ee5ef91ebeba5abe29f37d179fc72
Theo DUVAL <tduval@e3r12p22.42.fr>
2018-12-24 03:17:04 +0100

    had to norm it

--- srcs/arg_repl.c
+++ srcs/arg_repl.c
@@ -6,7 +6,7 @@
 /*   By: tduval <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/23 23:08:41 by tduval            #+#    #+#             */
-/*   Updated: 2018/12/23 23:53:51 by tduval           ###   ########.fr       */
+/*   Updated: 2018/12/24 03:16:35 by tduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,60 +17,49 @@
 static void	wave_case(char **argv, t_env *envi, int i)
 {
 	int		j;
-	char	*tmp2;
-	char	*tmp;
 	char	*om;
 
 	j = 0;
 	if (!(om = get_enval(envi, "HOME")))
 		return ;
-	while (argv[i][j])
-		j++;
-	if (!(tmp = ft_strnew(j)))
+	if (!(argv[i] = ft_strjoin(om, argv[i] + 1)))
 		return ;
-	j = -1;
-	while (argv[i][++j] != '~')
-		tmp[j] = argv[i][j];
-	if (!(tmp2 = ft_strjoin(tmp, om)))
-		return ;
-	free(tmp);
-	if (!(tmp = ft_strjoin(tmp2, argv[i] + j + 1)))
-		return ;
-	free(tmp2);
-	free(argv[i]);
-	if (!(argv[i] = ft_strdup(tmp)))
-		return ;
-	free(tmp);
 	free(om);
+}
+
+static void	dolar_case(char **argv, t_env *envi, int i)
+{
+	char	*tmp;
+
+	if (!(tmp = get_enval(envi, argv[i] + 1)))
+	{
+		free(argv[i]);
+		if (!(argv[i] = ft_strdup("")))
+			return ;
+	}
+	else
+	{
+		free(argv[i]);
+		if (!(argv[i] = ft_strdup(tmp)))
+		{
+			free(tmp);
+			return ;
+		}
+		free(tmp);
+	}
 }
 
 char		**arg_repl(char **argv, t_env *envi)
 {
 	int		i;
-	char	*tmp;
 
-	i = 0;
-	while (argv[i])
+	i = -1;
+	while (argv[++i])
 	{
 		if (argv[i][0] == '$')
-		{
-			if (!(tmp = get_enval(envi, argv[i] + 1)))
-			{
-				free(argv[i]);
-				if (!(argv[i] = ft_strdup("")))
-					return (0);
-			}
-			else
-			{
-				free(argv[i]);
-				if (!(argv[i] = ft_strdup(tmp)))
-					return (0);
-				free(tmp);
-			}
-		}
-		else if (ft_strchr(argv[i], '~'))
+			dolar_case(argv, envi, i);
+		else if (argv[i][0] == '~')
 			wave_case(argv, envi, i);
-		i++;
 	}
 	return (argv);
 }
