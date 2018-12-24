6d8e3bcf3afa334f58d10be9d3da29cb0e9ebde8
Theo DUVAL <tduval@e3r12p22.42.fr>
2018-12-24 03:26:25 +0100

    had to norm it

--- built-ins/env.c
+++ built-ins/env.c
@@ -6,7 +6,7 @@
 /*   By: tduval <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/22 21:19:34 by tduval            #+#    #+#             */
-/*   Updated: 2018/12/24 02:31:17 by tduval           ###   ########.fr       */
+/*   Updated: 2018/12/24 03:25:50 by tduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,31 @@
 #include "minishell.h"
 #include "libft.h"
 
-int		bi_env(char **argv, t_env *cpy)
+static void	arg_case(char **argv)
+{
+	pid_t		t;
+
+	if (argv[2] && ft_strequ(argv[1], "-i"))
+	{
+		t = fork();
+		if (t == 0)
+			execve(argv[2], argv, 0);
+		else if (t > 0)
+			t = wait(0);
+		else if (t == -1)
+			ft_printf("Couldn't fork on %s.\n", argv[1]);
+	}
+	else
+		ft_putendl("env: usage: env -i [command]");
+}
+
+int			bi_env(char **argv, t_env *cpy)
 {
 	int			i;
-	pid_t		t;
 
 	i = 0;
 	if (argv[1])
-	{
-		if (argv[2] && ft_strequ(argv[1], "-i"))
-		{
-			t = fork();
-			if (t == 0)
-				execve(argv[2], argv, 0);
-			else if (t > 0)
-				t = wait(0);
-			else if (t == -1)
-				ft_printf("Couldn't fork on %s.\n", argv[1]);
-		}
-		else
-		{
-			ft_putendl("env: usage: env -i [command]");
-			return (1);
-		}
-	}
+		arg_case(argv);
 	else
 	{
 		while (cpy)
