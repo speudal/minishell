01767bc8f10757df88afbd2e32253b08f6b30791
Theo DUVAL <tduval@e3r12p22.42.fr>
2018-12-24 01:16:41 +0100

    cd done

--- built-ins/cd.c
+++ built-ins/cd.c
@@ -6,7 +6,7 @@
 /*   By: tduval <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/23 03:58:04 by tduval            #+#    #+#             */
-/*   Updated: 2018/12/24 01:10:20 by tduval           ###   ########.fr       */
+/*   Updated: 2018/12/24 01:15:16 by tduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int			bi_cd(char **argv, t_env *envi)
 {
 	t_stat	buf;
 	char	*pothome;
-	char	*tmp;
+	char	path[256];
 
 	pothome = 0;
 	if (!argv[1])
@@ -30,35 +30,32 @@ int			bi_cd(char **argv, t_env *envi)
 			ft_putendl("cd: \"HOME\" variable not found.");
 			return (1);
 		}
-		tmp = get_enval(envi, "PWD");
-		ch_enval(envi, "OLDPWD",  tmp);
+		getcwd(path, 256);
+		ch_enval(envi, "OLDPWD",  path);
 		chdir(pothome);
 		ch_enval(envi, "PWD", pothome);
 		ft_strdel(&pothome);
-		ft_strdel(&tmp);
 	}
 	else
 	{
 		if (ft_strequ(argv[1], "-"))
 		{
 			pothome = get_enval(envi, "OLDPWD");
-			tmp = get_enval(envi, "PWD");
-			ch_enval(envi, "OLDPWD", tmp);
+			getcwd(path, 256);
+			ch_enval(envi, "OLDPWD", path);
 			chdir(pothome);
 			ch_enval(envi, "PWD", pothome);
 			ft_strdel(&pothome);
-		ft_strdel(&tmp);
 			return (1);
 		}
 		else if ((lstat(argv[1], &buf) == -1))
 			ft_printf("cd: no such file or directoy: %s\n", argv[1]);
 		else if (S_ISDIR(buf.st_mode))
 		{
-			pothome = get_enval(envi, "PWD");
-			ch_enval(envi, "OLDPWD", pothome);
+			getcwd(path, 256);
+			ch_enval(envi, "OLDPWD", path);
 			chdir(argv[1]);
 			ch_enval(envi, "PWD", argv[1]);
-			ft_strdel(&pothome);
 		}
 		else
 			ft_printf("cd: not a directory: %s\n", argv[1]);
