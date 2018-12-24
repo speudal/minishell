1dab760f047f47899b2c7242cebde110ba14de3e
Theo DUVAL <tduval@e3r12p22.42.fr>
2018-12-24 03:24:14 +0100

    had to norm it

--- built-ins/cd.c
+++ built-ins/cd.c
@@ -6,7 +6,7 @@
 /*   By: tduval <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/23 03:58:04 by tduval            #+#    #+#             */
-/*   Updated: 2018/12/24 02:04:30 by tduval           ###   ########.fr       */
+/*   Updated: 2018/12/24 03:23:44 by tduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,51 +16,67 @@
 #include "minishell.h"
 #include "libft.h"
 
-int			bi_cd(char **argv, t_env *envi)
+static void	no_mod_case(t_env *envi)
 {
-	t_stat	buf;
 	char	*pothome;
 	char	path[256];
 
-	pothome = 0;
-	if (!argv[1])
+	if (!(pothome = get_enval(envi, "HOME")))
+		ft_putendl("cd: \"HOME\" variable not found.");
+	else
 	{
-		if (!(pothome = get_enval(envi, "HOME")))
-		{
-			ft_putendl("cd: \"HOME\" variable not found.");
-			return (1);
-		}
 		getcwd(path, 256);
-		ch_enval(envi, "OLDPWD",  path);
+		ch_enval(envi, "OLDPWD", path);
 		chdir(pothome);
 		ch_enval(envi, "PWD", pothome);
 		ft_strdel(&pothome);
 	}
+}
+
+static void	less_case(t_env *envi)
+{
+	char	*pothome;
+	char	path[256];
+
+	if (!(pothome = get_enval(envi, "OLDPWD")))
+		return ;
+	getcwd(path, 256);
+	ch_enval(envi, "OLDPWD", path);
+	chdir(pothome);
+	ch_enval(envi, "PWD", pothome);
+	ft_strdel(&pothome);
+}
+
+static void	regular_case(char **argv, t_env *envi)
+{
+	char	path[256];
+
+	getcwd(path, 256);
+	if (chdir(argv[1]) == -1)
+		ft_printf("cd: permission denied: %s\n", argv[1]);
+	else
+	{
+		ch_enval(envi, "OLDPWD", path);
+		ch_enval(envi, "PWD", argv[1]);
+	}
+}
+
+int			bi_cd(char **argv, t_env *envi)
+{
+	t_stat	buf;
+	char	*pothome;
+
+	pothome = 0;
+	if (!argv[1])
+		no_mod_case(envi);
 	else
 	{
 		if (ft_strequ(argv[1], "-"))
-		{
-			pothome = get_enval(envi, "OLDPWD");
-			getcwd(path, 256);
-			ch_enval(envi, "OLDPWD", path);
-			chdir(pothome);
-			ch_enval(envi, "PWD", pothome);
-			ft_strdel(&pothome);
-			return (1);
-		}
+			less_case(envi);
 		else if ((lstat(argv[1], &buf) == -1))
 			ft_printf("cd: no such file or directoy: %s\n", argv[1]);
 		else if (S_ISDIR(buf.st_mode))
-		{
-			getcwd(path, 256);
-			if (chdir(argv[1]) == -1)
-				ft_printf("cd: permission denied: %s\n", argv[1]);
-			else
-			{
-				ch_enval(envi, "OLDPWD", path);
-				ch_enval(envi, "PWD", argv[1]);
-			}
-		}
+			regular_case(argv, envi);
 		else
 			ft_printf("cd: not a directory: %s\n", argv[1]);
 	}
